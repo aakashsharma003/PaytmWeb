59a4d9e94bd6156fe6f675f8bf20557f7befb17d
Akash Sharma <aakash6263264@gmail.com>
2024-03-29 13:04:41 +0530

    fixed user not found error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-export const server = "http://localhost:3000/api/v1";
+export const server = "https://paytmweb.onrender.com/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
