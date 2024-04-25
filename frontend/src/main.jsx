6c6d2d88ea2e59cff7ad0eba04d80db2e696b971
Akash Sharma <aakash6263264@gmail.com>
2024-04-25 13:05:10 +0530

    fixing backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 //http://localhost:3000/api/v1
 // https://paytmweb.onrender.com/api/v1
-export const server = "http://localhost:3000/api/v1";
+export const server = " https://paytmweb.onrender.com/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
