a3dd66a04b94de87ab11e95f25f16bbbafa400ae
aakashsharma003 <aakash6263264@gmail.com>
2024-06-30 22:31:01 +0530

    fixes pwa icon size

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
         src: 'Paytm-Logo.wine.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose:"any"
+        purpose:"any maskable"
       },
       {
         src: 'Paytm-Logo.wine.png',
