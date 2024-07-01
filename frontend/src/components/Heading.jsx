2598d475969046899a1956aec83437ab89a45c27
aakashsharma003 <aakash6263264@gmail.com>
2024-07-01 09:11:43 +0530

    fixing subheading

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -1,6 +1,6 @@
 const Heading = ({ label }) => {
   return (
-    <div className="text-4xl font-bold px-[1vw] py-[1vh] text-center italic">
+    <div className="text-4xl font-bold px-[1vw] py-[1vh] text-center">
       {label}
     </div>
   );
