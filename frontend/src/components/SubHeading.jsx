4a52e0f84355cb9c95f14d56ec95fd8031700ead
aakashsharma003 <aakash6263264@gmail.com>
2024-07-01 06:31:42 +0530

    fixing pwa icon

--- frontend/src/components/SubHeading.jsx
+++ frontend/src/components/SubHeading.jsx
@@ -1,6 +1,6 @@
 export const SubHeading = ({ label }) => {
   return (
-    <div className="text-base md:text-xl px-1 text-center text-black overflow-hidden whitespace-nowrap">
+    <div className="text-base md:text-lg px-1 text-center text-black overflow-hidden whitespace-nowrap">
       {label}
     </div>
   );
